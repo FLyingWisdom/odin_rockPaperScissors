346cc677d48c03a9f67d36b8c36d0ddc7fdb9207
FlyingWisdom <saobanpirzada@hotmail.com>
2023-07-18 19:38:48 +0100

    Created basic skeleton index.html and javascript.js, as well as plan for first two functions

---
+++ javascript.js
@@ -0,0 +1,14 @@
+console.log("Hello, World! This. is. Rock. Paper. Scissors!!!")
+
+//create a function called getComputerChoice
+// that will randomly generate a number between 1 and 3 and depending 
+// on that assign it rock, paper or scissors
+// the value that this returns is assigned as computerSelection
+
+
+// create a function that takes user input of rock, paper or scissors
+// this is playerSelection, player selection is cases insesnitive
+// the function then compares the two values and
+// using an if else statement (?) determines who wins
+// through a return statement of "you lose/win, x beats y"
+
